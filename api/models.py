1b58c3b49d2d06e0444028d771fb2a0c7242a00f
ksb0511 <soobin796@gmail.com>
2021-05-01 12:16:26 +0900

    signin/signup token complete

--- api/models.py
+++ api/models.py
@@ -1,22 +1,20 @@
 
 from django.db import models
 
-
-class model_user(models.Model):
+class user(models.Model):
     # user idx는 자동 생성
     user_id = models.TextField(max_length=100)
     user_pw = models.TextField(max_length=15)
 
-class model_analysis(models.Model):
-    # analysis idx는 자동 생성
-    user_idx = models.ForeignKey("model_user", related_name="model_user", on_delete=models.CASCADE, db_column="user_idx")
-    url = models.TextField(max_length=300)
-    title = models.TextField(max_length=200)
-    thumbnail = models.CharField(max_length=200)
-    analysis_date = models.DateField(auto_now=True)
-    channel_name = models.CharField(max_length=100)
-    video_time = models.CharField(max_length=20)
-    video_date = models.CharField(max_length=20)
-    topic = models.TextField(max_length=300)
-    graph_language = models.CharField(max_length=20)
-    graph_bad_comment = models.CharField(max_length=20)
+# class analysis(models.Model):
+#     # analysis idx는 자동 생성
+#     user_idx = models.ForeignKey("user", related_name="user", on_delete=models.CASCADE, db_column="user_idx")
+#     url = models.TextField(max_length=300)
+#     title = models.TextField(max_length=200)
+#     thumbnail = models.CharField(max_length=200)
+#     analysis_date = models.DateField(auto_now=True)
+#     channel_name = models.CharField(max_length=100)
+#     video_time = models.CharField(max_length=20)
+#     topic = models.TextField(max_length=300)
+#     graph_language = models.CharField(max_length=20)
+#     graph_bad_comment = models.CharField(max_length=20)
--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('user_test/', views.user_list), # 사용자 전체 정보 확인하는 테스트용 api
     path('signin/', views.SignIn.as_view()), # 로그인
     path('signup/', views.SignUp.as_view()), # 회원가입
-    path('history/<int:pk>/',views.history_list),
-    path('analysis/', views.analysis_view)
+    # path('history/<int:pk>/',views.history_list),
+    # path('analysis/', views.analysis_view),
+    path('ex/', views.ex),
 ]
