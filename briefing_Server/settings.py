83ff7cd5d6dcf8c7163d1ebeb4f252042cf0b684
subeen Kang <soobin796@gmail.com>
2021-05-25 15:11:03 +0900

    remove private info

--- briefing_Server/settings.py
+++ briefing_Server/settings.py
@@ -150,22 +150,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_STORAGE='intranet_clone.asset_storage.StaticStorage'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-AWS_ACCESS_KEY_ID = 'AKIAS35JB6P7Y6THCO6G'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_REGION = 'ap-northeast-2'
-
-###S3 Storages
-AWS_STORAGE_BUCKET_NAME = 'meshstt'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME,AWS_REGION)
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
